e7902528abfa6fb10dd0142b45e893735072992f
tracymedcalt <mtracy_22@yahoo.com>
2019-06-22 15:57:23 -0700

    not pretty but works

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
 let model = [];
+let stages = [];
 
-function addText() {
+function displayUnsorted() {
     let input = document.getElementById("textInput");
+    model.push(input.value);
+    document.getElementById("originalArray").innerHTML = model.join(', ');
+}
+
+function addP(text) {
     let p = document.createElement("P");
-    let text = input.value;
     let textNode = document.createTextNode(text);
     p.appendChild(textNode);
     document.body.appendChild(p);
-    model.push(text);
 }
 
-addText();
+function partition(arr, low, high) {
+    let pivot = arr[high];
+    let i = low - 1;
+    for (let j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+           [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    }
+    [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+    return i + 1;
+}
+
+function quicksort(arr, low, high) {
+    if (low < high) { 
+        addP(arr.join(', '));
+        let partitionIndex = partition(arr, low, high);
+        quicksort(arr, low, partitionIndex - 1);
+        quicksort(arr, partitionIndex + 1, high);
+    }
+}
+
 
 function sort() {
-    model.sort();
-    let paras = document.getElementsByTagName("P");
-    for (let i = 0; i < model.length; i++) {
-        paras[i].innerText = model[i];
-    }
+    quicksort(model, 0, model.length)
+    addP(model.join(', '));
 }
